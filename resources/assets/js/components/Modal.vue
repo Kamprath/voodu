0b68da1a53f71a87cabc61e0ce29f89a36532ced
Johnny Kamprath <johnnykamprath@gmail.com>
2017-11-13 19:21:44 -0600

    Add Modal component

---
+++ resources/assets/js/components/Modal.vue
@@ -0,0 +1,33 @@
+<template>
+
+    <div v-bind:class="{ 'modal': true, 'is-active': active }">
+        <div class="modal-background"></div>
+
+        <div class="modal-card">
+            <header class="modal-card-head">
+                <p class="modal-card-title">
+                    {{ title }}
+                </p>
+                <button class="delete" aria-label="close"></button>
+            </header>
+
+            <section class="modal-card-body">
+                <slot></slot>
+            </section>
+
+            <footer class="modal-card-foot">
+                <button class="button is-success">Save changes</button>
+                <button class="button">Cancel</button>
+            </footer>
+        </div>
+    </div>
+
+</template>
+
+<script>
+
+    export default {
+        props: ['active', 'title']
+    }
+
+</script>
