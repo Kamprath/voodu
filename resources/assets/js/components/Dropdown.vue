7697d126a2dc8d4d753382652f5ae90833e078ae
Johnny Kamprath <johnnykamprath@gmail.com>
2017-11-22 12:13:25 -0600

    Create Dropdown component

---
+++ resources/assets/js/components/Dropdown.vue
@@ -0,0 +1,34 @@
+<template>
+
+    <div :class="{ 'dropdown': true, 'is-active': isActive }">
+        <div class="dropdown-trigger">
+            <a class="icon is-small"
+               href="#"
+               @click.prevent="isActive = !isActive"
+               aria-haspopup="true">
+                <i class="fa fa-angle-down" aria-hidden="true"></i>
+            </a>
+        </div>
+
+        <div class="dropdown-menu" role="menu">
+            <div class="dropdown-content">
+                <slot></slot>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+
+    export default {
+
+        data() {
+            return {
+                isActive: false
+            };
+        }
+
+    }
+
+</script>
