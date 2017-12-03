27821b665fff93c109a804fea1031511af31aac9
Johnny Kamprath <johnnykamprath@gmail.com>
2017-12-03 00:54:25 -0600

    Add /logout route and change post-login redirect

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 Auth::routes();
 
+Route::get('/logout', function() {
+    Auth::logout();
+    return redirect('/login');
+});
+
 Route::get('/', 'AppController@index');
 
 Route::get('/boards/{id}', 'AppController@index');
