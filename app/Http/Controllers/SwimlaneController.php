889eefed18476c6caf3a09e55f44ed96f85a6e82
Johnny Kamprath <johnnykamprath@gmail.com>
2017-12-04 14:28:28 -0600

    Add controllers for Card and Swimlane resources

---
+++ app/Http/Controllers/SwimlaneController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Swimlane;
+use Illuminate\Http\Request;
+
+class SwimlaneController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Swimlane  $swimlane
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Swimlane $swimlane)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Swimlane  $swimlane
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Swimlane $swimlane)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Swimlane  $swimlane
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Swimlane $swimlane)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Swimlane  $swimlane
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Swimlane $swimlane)
+    {
+        //
+    }
+}
