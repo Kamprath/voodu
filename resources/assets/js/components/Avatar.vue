5dd8fbd6e8891db4c8aafd37dce4f51cc9e9e775
Johnny Kamprath <johnnykamprath@gmail.com>
2018-01-02 15:47:17 -0600

    Create Avatar component for user/team avatars

---
+++ resources/assets/js/components/Avatar.vue
@@ -0,0 +1,42 @@
+<template>
+    <figure class="avatar" :style="style">
+        <span v-if="!src" class="avatar-letters"></span>
+    </figure>
+</template>
+
+<script>
+    export default {
+        props: {
+            src: {
+                type: String,
+                default: null
+            },
+        },
+        computed: {
+            style() {
+                return 'background-image: url(\'' + this.src + '\');';
+            }
+        },
+    }
+</script>
+
+<style lang="less" scoped>
+    figure {
+        width: 2rem;
+        height: 2rem;
+        border-radius: 50%;
+        background-repeat: no-repeat;
+        background-position: center center;
+        background-color: rgba(71, 71, 71, 0.4);
+        background-size: cover;
+
+        &.is-small {
+            width: 1.5rem;
+            height: 1.5rem;
+        }
+        &.is-large {
+            width: 3rem;
+            height: 3rem;
+        }
+    }
+</style>
