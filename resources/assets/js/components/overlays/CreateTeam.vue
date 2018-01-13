2ab7552ec9d23a57d6c5392a4b990592c8f66763
Johnny Kamprath <johnnykamprath@gmail.com>
2018-01-13 15:28:47 -0600

    Fix Stripe instantiation bug

--- resources/assets/js/components/overlays/CreateTeam.vue
+++ resources/assets/js/components/overlays/CreateTeam.vue
@@ -304,7 +304,7 @@
                 step: 1,
                 type: 'monthly',
                 stripe: {
-                    instance: Stripe('pk_test_E6SDcp9h0sKSEqHQj2LBUYAc'),
+                    instance: null,
                     card: null,
                     token: null,
                     error: null
@@ -428,6 +428,8 @@
              * Render the Stripe card element.
              */
             initStripe() {
+                this.stripe.instance = Stripe('pk_test_E6SDcp9h0sKSEqHQj2LBUYAc');
+
                 this.stripe.card = this.stripe.instance
                     .elements()
                     .create('card');
