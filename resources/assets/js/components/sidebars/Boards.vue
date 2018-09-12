d0b8f11d5edfffe4891df46af8fe5ac00645ee4c
Johnny <johnny@classbundl.com>
2018-09-12 10:19:11 -0500

    Update X icon in sidebar

--- resources/assets/js/components/sidebars/Boards.vue
+++ resources/assets/js/components/sidebars/Boards.vue
@@ -4,7 +4,20 @@
 
         <!-- Close button -->
         <a href="#" class="close-sidebar" @click.prevent="$store.dispatch('hideSidebar')">
-            <i class="fa fa-times is-dark" aria-hidden="true"></i>
+            <svg version="1.1"
+                 xmlns="http://www.w3.org/2000/svg"
+                 xmlns:xlink="http://www.w3.org/1999/xlink"
+                 x="0px"
+                 y="0px"
+                 width="20px"
+                 height="20px"
+                 viewBox="0 0 357 357"
+                 style="enable-background:new 0 0 357 357;opacity: .45;"
+                 xml:space="preserve">
+                <g>
+                <polygon points="357,35.7 321.3,0 178.5,142.8 35.7,0 0,35.7 142.8,178.5 0,321.3 35.7,357 178.5,214.2 321.3,357 357,321.3 214.2,178.5"/>
+                </g>
+            </svg>
         </a>
 
         <h5 class="title is-5">Boards</h5>
